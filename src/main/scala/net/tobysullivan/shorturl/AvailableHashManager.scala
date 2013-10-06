2571ec474d5ba0cbe5b915f6c5797a06699b497f
Toby Sullivan <tobyjsullivan@gmail.com>
2013-10-06 02:27:01 -0700

    Removed an unneccissary synchronized section

--- src/main/scala/net/tobysullivan/shorturl/AvailableHashManager.scala
+++ src/main/scala/net/tobysullivan/shorturl/AvailableHashManager.scala
@@ -11,12 +11,10 @@ import akka.agent.Agent
 object AvailableHashManager {
   /**
    * This method returns the next available hash as an Int in a thread-safe manner.
-   */  
+   */
   private val cursor = Agent(HashStore.findNextAvailableHash)
   def getNext(): Int = {
-    synchronized {
-      val future = cursor alter (_ + 1)
-      Await.result(future, 1 second)
-    }
+    val future = cursor alter (_ + 1)
+    Await.result(future, 1 second)
   }
 }
